6a97eb10875e95db1102e8a818b79e2c1cd243ab
adithyasamudrala <adithya.samudrala@gmail.com>
2023-11-19 13:22:21 +0800

    adding cli functionality

--- LoginManager.java
+++ LoginManager.java
@@ -1,3 +1,12 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class LoginManager {
     public static User obtainUserObject(String email, String password) {
         //check if user is an ccm or attendee
